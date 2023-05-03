cd2aa3b5cb415f54a6f5951d6150a37438a442bb
pavel kuzmin <pavelwebdev26@yandex.ru>
2023-05-03 19:01:48 +0300

    fixed open and close btn

--- js/overlay.js
+++ js/overlay.js
@@ -2,10 +2,12 @@ const openBtn = document.querySelector('#openBtn');
 const closeBtn = document.querySelector('#closeBtn');
 const overlayElement = document.querySelector('.fullscreen-menu');
 
-openBtn.addEventListener('click', function(){
+openBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
     overlayElement.classList.add('fullscreen__active')
 });
 
-closeBtn.addEventListener('click', function(){
+closeBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
     overlayElement.classList.remove('fullscreen__active')
 });
